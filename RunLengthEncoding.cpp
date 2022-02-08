2402fc90dd1bda088ce12d50b8e19ace0163b84e
marc <78002988+novak-99@users.noreply.github.com>
2022-02-08 16:30:19 +0000

    Add files via upload

---
+++ RunLengthEncoding.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <map>
+
+ std::map<char, int> RLE(std::string &data){ // Data is in form of str.
+    std::map<char, int> compressedData; 
+    for(int i = 0; i < data.length(); i++){
+        if(i > 0 && data[i] == data[i - 1]){
+            compressedData[data[i]]++;
+            continue; 
+        }
+        compressedData.insert(std::pair<char,int>(data[i], 1));
+    }
+    return compressedData;
+}
+
+void printData(std::map<char, int>& compressedData){
+    for(auto [m, v] : compressedData){
+        std::cout << m << v;
+    }
+    std::cout << std::endl;
+}
+
+int main(){
+    std::string data("aaaabbbbccccddd");
+    auto compressedData = RLE(data);
+    printData(compressedData);
+    return 0; 
+}
