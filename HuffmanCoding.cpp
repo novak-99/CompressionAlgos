a382bd859b032f1127275508163f96237810f511
novak_99 <cooljohn312@gmail.com>
2022-02-25 15:34:27 -0800

    huffman coding => done

--- HuffmanCoding.cpp
+++ HuffmanCoding.cpp
@@ -46,50 +46,111 @@ double sumAllElements(std::vector<double>& vec){
     return sum;
 }
 
-int findMaxElement(std::vector<double>& vec){
-    return std::max_element(vec.begin(), vec.end()) - vec.begin();
+int findMinElement(std::vector<double>& vec){
+    return std::min_element(vec.begin(), vec.end()) - vec.begin();
+}
+
+void preOrderSearch(Node* root, std::map<char, std::string>& huffmanEncodings, std::string& encodingStr){ // Our encodings are strings. (can convert.)
+    // NOTE:
+    // LEFT => 1
+    // RIGHT => 0
+
+    // If our current node is null at left, it's empty and we don't search it. 
+    if (root->getLeft() == nullptr) { 
+        huffmanEncodings.insert(std::pair<char,std::string>(root->getData(), encodingStr)); // switch with coding
+        return; 
+    } 
+
+    encodingStr += "1";
+    preOrderSearch(root->getLeft(), huffmanEncodings, encodingStr);
+    encodingStr.pop_back();
+
+    // If our current node is null at right, it's empty and we don't search it. 
+    if (root->getRight() == nullptr) { 
+        huffmanEncodings.insert(std::pair<char,std::string>(root->getData(), encodingStr));
+        return; 
+    } 
+
+    encodingStr += "0";
+    preOrderSearch(root->getRight(), huffmanEncodings, encodingStr);
+    encodingStr.pop_back();
 }
 
 // More efficient to use map, but two vectors were used to simplify vector operations. 
 Node* createHuffmanTree(std::map<int, char> data, std::vector<double> frequencies){
-    double sum = sumAllElements(frequencies);
-    Node* root = new Node(sum);
-
-    std::vector<Node*> rightTree; 
-    std::vector<Node*> leftTree;
-
-    leftTree.push_back(root);
+    std::vector<Node*> currentTree; 
 
     for(int i = 0; !frequencies.empty(); i++){
 
-        int maxIndex = findMaxElement(frequencies);
-        sum -= frequencies[maxIndex];
-
-        rightTree.push_back(new Node(frequencies[maxIndex]));
-        rightTree[rightTree.size() - 1]->setData(data[frequencies[maxIndex]]);
-
-        leftTree.push_back(new Node(frequencies[maxIndex]));
-        leftTree[leftTree.size() - 2]->setLeft(leftTree[leftTree.size() - 1]);
-        leftTree[leftTree.size() - 2]->setRight(rightTree[rightTree.size() - 1]);
-
-        frequencies.erase(frequencies.begin() + maxIndex);
-
-        if(frequencies.size() == 1){ 
-            int maxIndex = findMaxElement(frequencies);
-            leftTree[leftTree.size() - 1]->setData(data[frequencies[maxIndex]]);
-            std::cout << leftTree.size() << std::endl;
-            std::cout << rightTree.size() << std::endl;
-            break;
+        int firstMinIndex = findMinElement(frequencies);
+        Node* currentNodeLeft = new Node(frequencies[firstMinIndex]);
+        // Check if this node already exists, but only when epoch > 0 to avoid seg fault.
+        if(i > 0){
+            for(int i = 0; i < currentTree.size(); i++){
+                if(currentTree[i]->getFrequency() == frequencies[firstMinIndex]){
+                    currentNodeLeft = currentTree[i];
+                    break;
+                }
+            }
         }
+
+        frequencies.erase(frequencies.begin() + firstMinIndex);
+
+        // This conditional finds if frequency-datum pair exists in map. 
+        // If not, there is no need to set the data field. 
+        if(data.find(currentNodeLeft->getFrequency()) != data.end()){ 
+            currentNodeLeft->setData(data[currentNodeLeft->getFrequency()]);
+        }
+
+        int secondMinIndex = findMinElement(frequencies);
+        Node* currentNodeRight = new Node(frequencies[secondMinIndex]); // fr[i_2] > fr[i_1]. Greater always on right. 
+        // Check if this node already exists, but only when epoch > 0 to avoid seg fault.
+        if(i > 0){
+            for(int i = 0; i < currentTree.size(); i++){
+                if(currentTree[i]->getFrequency() == frequencies[secondMinIndex]){
+                    currentNodeRight = currentTree[i];
+                    break;
+                }
+            }
+        }
+
+        frequencies.erase(frequencies.begin() + secondMinIndex);
+
+        // This conditional finds if frequency-datum pair exists in map. 
+        // If not, there is no need to set the data field. 
+        if(data.find(currentNodeRight->getFrequency()) != data.end()){ 
+            currentNodeRight->setData(data[currentNodeRight->getFrequency()]);
+        }
+
+        int summedFreq = currentNodeLeft->getFrequency() + currentNodeRight->getFrequency();
+        // Pushing back to empty vector exacts a seg fault. This conditional avoids that. 
+        if(!frequencies.empty()) 
+            frequencies.push_back(summedFreq);
+        Node* currentNode = new Node(summedFreq); 
+
+        currentNode->setLeft(currentNodeLeft);
+
+        currentNode->setRight(currentNodeRight);
+
+        currentTree.push_back(currentNode);
     }
-    return root;
+    return currentTree[currentTree.size() - 1]; // The final index must be root. 
 }
 
 int main(){
     std::map<int, char> data; 
-    data.insert(std::pair<int,char>(1, 'a'));
-    data.insert(std::pair<int,char>(78, 'b'));
-    data.insert(std::pair<int,char>(5, 'c'));
-    Node* root = createHuffmanTree(data, {1, 78, 5});
-    std::cout << root->getRight()->getData() << std::endl;
+    data.insert(std::pair<int,char>(5, 'a'));
+    data.insert(std::pair<int,char>(6, 'b'));
+    data.insert(std::pair<int,char>(7, 'c'));
+    data.insert(std::pair<int,char>(8, 'd'));
+    Node* root = createHuffmanTree(data, {5, 6, 7, 8});
+    std::cout << root->getRight()->getRight()->getData() << std::endl;
+
+    std::map<char, std::string> codings; 
+    std::string encodingStr = "";  // Init it.
+    preOrderSearch(root, codings, encodingStr);
+
+    for(auto [m, v] : codings){
+        std::cout << m << ":" << v << std::endl;
+    }
 }
